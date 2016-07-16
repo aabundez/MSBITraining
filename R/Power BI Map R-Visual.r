ec43bef5df0d07dacbd94413f54915d047db1b62
Angel Abundez <angel@designmind.com>
2016-07-15 18:43:15 -0700

    Reordered the drawing of origins last. Corrected bubbles of flights at destination.

--- R/Power BI Map R-Visual.r
+++ R/Power BI Map R-Visual.r
@@ -1,11 +1,10 @@
 # Create dataframe
-# dataset <- data.frame(DEST, Departures Performed, Load Factor, Load Factor by Origin, OriginLat, OriginLon, DestinationLat, DestinationLon, Airport, Origin)
+# dataset <- data.frame(DestinationLat, DestinationLon, DEST, Load Factor, Load Factor by Origin, OriginLat, OriginLon, Origin, Departures Performed, Airport)
 
 #Remove duplicated rows
 #dataset <- unique(dataset)
 
 # TODO:
-#	Draw points and text after lines, and only as many times as necessary (separate for loop)
 #   Bubble Legend
 #   Remove Color Palette for Points. Using alpha value to identify patterns when overplotting points.
 
@@ -13,13 +12,13 @@ library(maps)		# for map
 library(geosphere)	# for great circle points
 library(mapproj)	# for points
 
+
+# Calculate best map limits to maintain 2:1 aspect ratio
+
 lat <- c(dataset$OriginLat, dataset$DestinationLat)
 lon <- c(dataset$OriginLon, dataset$DestinationLon)
+viewport_angle <- 63.4349 * (pi/180)                               # angle b/t left side of viewport and diagnoal (hypotenuse)
 
-#do a bit of Geometry
-viewport_angle <- 63.4349 * (pi/180)
-
-# finding bounding box points
 if ( (max(lat) - min(lat)) / (max(lon) - min(lon)) >= .5 ) {
   x <- c( (    ( max(lon) - ( (max(lon) - min(lon)) / 2 ) )    -    ( ((max(lat)+1) - (min(lat)-1)) / 2 ) * tan(viewport_angle )      ) ,
              (    ( max(lon) - ( (max(lon) - min(lon)) / 2 ) )    +    ( ((max(lat)+1) - (min(lat)-1)) / 2 ) * tan(viewport_angle )      )
@@ -32,7 +31,7 @@ y <- c( (    ( max(lat) - ( (max(lat) - min(lat)) / 2 ) )    -    ( ((max(lon)+5
           )
 }
 
-# create map and points
+# Draw world map with states and scale
 map("world", col="#F1F1F1", fill=TRUE, bg="#FFFFFF", lwd=0.05, xlim=x, ylim=y, mar=rep(0,4))   # map w/ zoom
 map("state", col="#d2d2d2", fill = FALSE, add = TRUE)
 map.scale(grconvertX(0.01, "npc"), grconvertY(0.07, "npc"), metric = FALSE, relwidth = .18, ratio = FALSE)
@@ -52,26 +51,45 @@ pcol <- "#E74C3C"
 # text color
 tcol <-"#002F2F"
 
-# order data set (to draw in order of palette shade)
+# Order dataset (to draw in order of palette shade)
 dataset <- dataset[order(dataset$"Load Factor by Origin"),]
 maxcnt <- max(dataset$"Load Factor by Origin")
 
-# define and draw lines
+# Draw Flight Routes
 for (j in 1:nrow(dataset)) {
     inter <- gcIntermediate( c(dataset$OriginLon[j], dataset$OriginLat[j]), c(dataset$DestinationLon[j], dataset$DestinationLat[j]), n=100, addStartEnd=TRUE)
     colindex <- round( (dataset$"Load Factor by Origin"[j] / maxcnt) * length(ln_col) )
     lines(inter, col=ln_col[colindex], lwd=1 ) # Draw lines
-
-    # OLD CODE:
-    # lines(inter, col=ln_col[colindex], lwd=colindex/10 )
     
-    points(mapproject(dataset$OriginLon[j], dataset$OriginLat[j]), col="#ff9147", bg="#FFED47", pch=25, cex=1)		# Origin point
-    points(mapproject(dataset$DestinationLon[j], dataset$DestinationLat[j]), col="#4C4D8B", bg=rgb( red=132, green=134, blue=242, alpha = 125, max=255 ), pch=21, cex= sqrt( dataset$"Departures Performed"[j] ) / 4 )		# Destination point
-
-    text(dataset$OriginLon[j], dataset$OriginLat[j], dataset$Airport[j], cex=1, pos=4, col=tcol)
-    text(dataset$DestinationLon[j], dataset$DestinationLat[j], dataset$DEST[j], cex=1, pos=4, col=tcol)
+    # points(mapproject(dataset$OriginLon[j], dataset$OriginLat[j]), col="#ff9147", bg="#FFED47", pch=25, cex=1)		# Origin point
+    # text(dataset$OriginLon[j], dataset$OriginLat[j], dataset$Airport[j], cex=1, pos=4, col=tcol)                                             # Origin airport code text
 }
 
+# Sum Flights by Destination
+dataset2 <- aggregate(dataset$"Departures Performed", by=list(dataset$"DEST", dataset$"DestinationLat", dataset$"DestinationLon"), FUN=sum)
+names(dataset2) <- c("DEST", "DestinationLat", "DestinationLon", "Departures by Destination")  # change column names
+
+# Draw bubbles for Flights by Destination
+for (a in 1:nrow(dataset2)) {
+    
+    if ( max(dataset2$"Departures by Destination"[a])  < 500 ) {
+        bubble_size_factor <- 5 
+    } else {
+        bubble_size_factor <- 10
+    } 
+
+    points(mapproject(dataset2$DestinationLon[a], dataset2$DestinationLat[a]), col="#4C4D8B", bg=rgb( red=132, green=134, blue=242, alpha = 125, max=255 ), pch=21, cex= sqrt( dataset2$"Departures by Destination"[a] ) / bubble_size_factor ) # Destination point
+    text(dataset2$DestinationLon[a], dataset2$DestinationLat[a], dataset2$DEST[a], cex=1, pos=4, col=tcol)  #Destination airport text
+}
+
+# Draw Origin Marks
+for (j in 1:nrow(dataset)) {
+    
+    points(mapproject(dataset$OriginLon[j], dataset$OriginLat[j]), col="#ff9147", bg="#FFED47", pch=25, cex=1)		# Origin point
+    text(dataset$OriginLon[j], dataset$OriginLat[j], dataset$Airport[j], cex=1, pos=4, col=tcol)                                             # Origin airport code text
+}
+
+# Add Marks Legend
 legend(
    grconvertX(0.01, "npc"), 
    grconvertY(0.15, "npc"),
